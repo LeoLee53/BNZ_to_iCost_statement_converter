2f3bc01cb95b10cae08a2c7131e35307d8c80646
Leo Lee <a1442963708@gmail.com>
2024-03-12 10:45:50 +1300

    update the COLUMNS variable to HEADERS in config.py file

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ This file contains the configuration for the BNZ_to_iCost_statement_converter.
 ICOST_FILE_PATH = '/Users/leolee/Desktop/coding/BNZ_to_iCost_statement_converter/test_materials/iCost.csv'
 BNZ_FILE_PATH = '/Users/leolee/Desktop/coding/BNZ_to_iCost_statement_converter/test_materials/BNZ.csv'
 DST_PATH = '/Users/leolee/Desktop/coding/BNZ_to_iCost_statement_converter/test_materials/test.csv'
-COLUMNS = ['Date', 'Type', 'Amount', 'First-Level Category', 'Second-Level Category', 'Account 01', 'Account 02',
+HEADERS = ['Date', 'Type', 'Amount', 'First-Level Category', 'Second-Level Category', 'Account 01', 'Account 02',
            'Remark', 'Currency', 'Tag', 'Book', 'Address', 'Refund', 'Discount', 'Fee', '图片链接1', '图片链接2',
            '图片链接3', 'Attachment 01', 'Attachment 02', 'Attachment 03']
