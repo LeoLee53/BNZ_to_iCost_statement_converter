29588dd4816682acddf416c09f131e7b1686774f
Leo Lee <a1442963708@gmail.com>
2024-03-12 14:50:49 +1300

    optimized create_template_dataframe and extract_bnz_columns function and fixed a bug where dictionary item adding is incorrectly implemented.

--- src/BNZ_to_iCost_converter.py
+++ src/BNZ_to_iCost_converter.py
@@ -2,24 +2,25 @@ import pandas as pd
 
 
 def create_template_dataframe(headers) -> pd.DataFrame:
-    if headers == None:
+    if headers is None:
         raise TypeError("Headers cannot be None")
 
     return pd.DataFrame(columns=headers)
 
 
 def extract_bnz_columns(bnz_file_path, columns: list[str]) -> dict:
-    data_frame = pd.read_csv(bnz_file_path)
+    bnz_data_frame = pd.read_csv(bnz_file_path)
 
     column_datas = dict()
     for column in columns:
-        column_datas[column] = data_frame[column]
+        if column not in bnz_data_frame.columns:
+            raise KeyError(f"Column '{column}' does not exist in the BNZ data frame")
+
+        column_datas[column] = bnz_data_frame[column]
 
     return column_datas
 
 
-def write_columns(serieses: list[pd.Series], template_data_frame):
-    for series in serieses:
-        if series.name is None:
-            raise AttributeError("Series must have a name")
-        template_data_frame[series.name] = series
+def write_columns(serieses: dict, template_data_frame):
+    for header in serieses:
+        template_data_frame[header] = serieses[header]
