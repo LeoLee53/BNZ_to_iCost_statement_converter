e8b29a50ba331a50fb67c74e811041ce38d88485
Leo Lee <a1442963708@gmail.com>
2024-03-12 15:55:47 +1300

    add a new function for writing data in a populated template dataframe into a csv file.

--- src/BNZ_to_iCost_converter.py
+++ src/BNZ_to_iCost_converter.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 
@@ -24,3 +26,11 @@ def extract_bnz_columns(bnz_file_path, columns: list[str]) -> dict:
 def write_columns(serieses: dict, template_data_frame):
     for header in serieses:
         template_data_frame[header] = serieses[header]
+
+
+def write_to_csv(populated_df: pd.DataFrame, dst_path: str):
+    dir_name = os.path.dirname(dst_path)
+    if not os.path.exists(dir_name):
+        raise FileNotFoundError(f"Directory '{dir_name}' does not exist")
+
+    populated_df.to_csv(dst_path, index=False, header=True)
