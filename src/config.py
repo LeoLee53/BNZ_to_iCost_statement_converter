2a33fbc03e30af339626682abe586eb3f265fa07
Leo Lee <a1442963708@gmail.com>
2024-03-14 20:31:38 +1300

    Update configuration file to work hand in hand with the refactoring of CSV_manipulation.py file.

--- src/config.py
+++ src/config.py
@@ -5,6 +5,16 @@ This file contains the configuration for the BNZ_to_iCost_statement_converter.
 ICOST_FILE_PATH = '/Users/leolee/Desktop/coding/BNZ_to_iCost_statement_converter/test_materials/iCost.csv'
 BNZ_FILE_PATH = '/Users/leolee/Desktop/coding/BNZ_to_iCost_statement_converter/test_materials/BNZ.csv'
 DST_PATH = '/Users/leolee/Desktop/coding/BNZ_to_iCost_statement_converter/test_materials/test.csv'
-HEADERS = ['Date', 'Type', 'Amount', 'First-Level Category', 'Second-Level Category', 'Account 01', 'Account 02',
-           'Remark', 'Currency', 'Tag', 'Book', 'Address', 'Refund', 'Discount', 'Fee', '图片链接1', '图片链接2',
-           '图片链接3', 'Attachment 01', 'Attachment 02', 'Attachment 03']
+HEADERS = ['日期', '类型', '金额', '一级分类', '二级分类', '账户1', '账户2', '备注', '货币', '标签']
+ENG_TO_CN_HEADER_MAP = {
+    'Payee': '备注',
+    'Amount': '金额',
+    'Date': '日期',
+    'This Party Account': '账户1',
+    'Other Party Account': '账户2'
+}
+
+BNZ_TO_ICOST_ACCOUNT_MAP = {
+    '02-0810-0146802-00': 'BNZ 4042',
+    '02-0865-0100529-00': 'Joint account'
+}
